1c632f29b37b799e1c8c0463b1956712ee29b890
samicrafter <sohailminecrafter@gmail.com>
2025-08-28 00:26:13 +0530

    add final

--- src/main/java/org/example/smarthome/Main.java
+++ src/main/java/org/example/smarthome/Main.java
@@ -12,19 +12,15 @@ public class Main {
         Light light = new Light("Living Room Light");
         Scanner sc = new Scanner(System.in);
         while (true) {
-            System.out.println("\n1. Turn ON Fan");
-            System.out.println("2. Turn OFF Fan");
-            System.out.println("3. Turn ON Light");
-            System.out.println("4. Turn OFF Light");
-            System.out.println("6. Exit");
+            System.out.println("\n1. Turn ON Light");
+            System.out.println("2. Turn ON Light");
+            System.out.println("3. Exit");
             System.out.print("Choose: ");
             int choice = sc.nextInt();
             switch (choice) {
-                case 1 -> fan.turnOn();
-                case 2 -> fan.turnOff();
-                case 3 -> light.turnOn();
-                case 4 -> light.turnOff();
-                case 6 -> {
+                case 1 -> light.turnOn();
+                case 2 -> light.turnOff();
+                case 3 -> {
                     System.out.println("Goodbye!");
                     return;
                 }
